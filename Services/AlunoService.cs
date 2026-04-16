748cede40161e561cd029b43533b91bc8693f1ba
Magalhaes de Lemos <66646459+MexLemos@users.noreply.github.com>
2026-04-16 08:47:21 +0100

    Add class e Service Aluno

---
+++ Services/AlunoService.cs
@@ -0,0 +1,11 @@
+public class AlunoService
+{
+    private List<Aluno> alunos= new ();
+    public List<Aluno> GetAll(){return alunos;}
+
+    public void Add(Aluno aluno)
+    {
+        aluno.Add(aluno);
+    }
+    
+}
