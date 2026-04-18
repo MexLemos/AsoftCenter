b5cff5b7ba235764082990a731f49e69ebf09d14
Magalhaes de Lemos <66646459+MexLemos@users.noreply.github.com>
2026-04-18 21:29:51 +0100

    Crud experimental sem BD

--- Services/AlunoService.cs
+++ Services/AlunoService.cs
@@ -1,11 +1,24 @@
 public class AlunoService
 {
     private List<Aluno> alunos= new ();
-    public List<Aluno> GetAll(){return alunos;}
+    public List<Aluno> GetAll()
+    {
+        return alunos;
+    }
 
     public void Add(Aluno aluno)
     {
-        aluno.Add(aluno);
+        alunos.Add(aluno);
     }
+    
+    private void Excluir(Aluno aluno)
+    {
+        alunos.Remove(aluno);
+    }    
+    public Aluno Get(int index)
+    {
+        return alunos[index];
+    }
+    
     
 }
--- obj/Debug/net10.0/AsoftCenter.AssemblyInfo.cs
+++ obj/Debug/net10.0/AsoftCenter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AsoftCenter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+748cede40161e561cd029b43533b91bc8693f1ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("AsoftCenter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AsoftCenter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
